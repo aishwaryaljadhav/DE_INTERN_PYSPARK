06b31d277f2f9d6b859a66d30c07b3d30f9b5259
aishwaryaljadhav <aishwaryaljadhav18@gmail.com>
2026-03-11 19:11:15 +0530

    added dataset to practise conversion functions

--- src/conversion_func/conv_dataset.py
+++ src/conversion_func/conv_dataset.py
@@ -1,14 +1,24 @@
 from pyspark.sql import SparkSession
 
 
-spark = SparkSession.builder.appName("MathDataset").getOrCreate()
+spark = SparkSession.builder.appName("ConvDataset").getOrCreate()
 
 data = [
-(1,"Laptop","70000","2025-03-01"),
-(2,"Mobile","25000","2025-03-05"),
-(3,"Shoes","3000","2025-03-08")
+(101,"John","45000","4.5","2025-03-01","true","2"),
+(102,"Alice","52000","3.9","2025-03-03","false","5"),
+(103,"Bob","39000","4.2","2025-03-05","true","1"),
+(104,"Emma","61000","4.8","2025-03-07","true","3"),
+(105,"David","47000","3.7","2025-03-09","false","4")
 ]
 
-columns = ["ID","Product","Price","Order_Date"]
+columns = [
+"Emp_ID",
+"Name",
+"Salary",
+"Rating",
+"Join_Date",
+"Active",
+"Projects"
+]
 
 df = spark.createDataFrame(data,columns)
